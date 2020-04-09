b0b9518f6c8d47150369c05ff189b6d157858f7c
Nicolas Tomassi <xtomasix@gmail.com>
2020-04-09 12:26:44 -0300

    se corrige buscador de perros

--- src/Dogs/usecases/DogController.ts
+++ src/Dogs/usecases/DogController.ts
@@ -46,7 +46,7 @@ class DogController {
     }
 
     filterDogs(inputDog : string) : Array<Dog> {
-        const filterDogs = this.dogList.filter(dog => (dog.fullName().indexOf(inputDog.toLowerCase()) > -1));
+        const filterDogs = this.dogList.filter(dog => (dog.fullName().toLowerCase().indexOf(inputDog.toLowerCase()) > -1));
         return filterDogs;
     }
 }
